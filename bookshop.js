e7f3354d2454b8e4d6d10894ad08d66ac0b75753
Ashleigh McMahon <ashleighmc@me.com>
2024-03-28 14:18:18 -0600

    help

--- bookshop.js
+++ bookshop.js
@@ -1,10 +1,17 @@
-function navigateToPage() {
-    // You can add additional JavaScript logic here if needed
-    // For example, perform some actions before navigating
-    console.log('Navigating to another page...');
+function showPopup() {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'block';
+    document.addEventListener('click', closePopupOutside);
 }
-
-function closeWindow() {
-    window.location.href='./index.html';
-    window.close(); // Close the current window
+function closePopup() {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'none';
+    document.removeEventListener('click', closePopupOutside);
+}
+function closePopupOutside(event) {
+    const popup = document.getElementById('popup');
+    const clickTarget = event.target;
+    if (!popup.contains(clickTarget)) {
+        closePopup(); // Close the popup if outside of the target window
+    }
 }
