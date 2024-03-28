d67eb88001f9294db1d1e0e9c5022e1c2b31350e
Ashleigh McMahon <ashleighmc@me.com>
2024-03-27 21:33:03 -0600

    Initital Commit

---
+++ bookshop.js
@@ -0,0 +1,10 @@
+function navigateToPage() {
+    // You can add additional JavaScript logic here if needed
+    // For example, perform some actions before navigating
+    console.log('Navigating to another page...');
+}
+
+function closeWindow() {
+    window.location.href='./index.html';
+    window.close(); // Close the current window
+}
